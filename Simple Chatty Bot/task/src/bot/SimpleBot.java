2b4336c594e28c20fbab19c8e7214d0e9634ac4a
bklimkeit <bernd@klimke-it.de>
2019-08-17 16:57:36 +0200

    Updated variable names.

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -28,10 +28,10 @@ public class SimpleBot {
     private static void guessAge() {
         System.out.println("Let me guess your age.");
         System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
-        int rem3 = scanner.nextInt();
-        int rem5 = scanner.nextInt();
-        int rem7 = scanner.nextInt();
-        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        int remainderBy3 = scanner.nextInt();
+        int remainderBy5 = scanner.nextInt();
+        int remainderBy7 = scanner.nextInt();
+        int age = (remainderBy3 * 70 + remainderBy5 * 21 + remainderBy7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 
@@ -39,7 +39,7 @@ public class SimpleBot {
         System.out.println("Now I will prove to you that I can count to any number you want.");
         int num = scanner.nextInt();
         for (int i = 0; i <= num; i++) {
-            System.out.printf("%d!\n", i);
+            System.out.println(i + "!");
         }
     }
 
@@ -59,6 +59,6 @@ public class SimpleBot {
     }
 
     private static void end() {
-        System.out.println("Congratulations, have a nice day!"); // Do not change this text
+        System.out.println("Congratulations, have a nice day!");
     }
 }
